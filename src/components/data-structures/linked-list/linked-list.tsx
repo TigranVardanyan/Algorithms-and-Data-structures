3a728047c491e0629f8ccf1862a51a88c657a467
Tigran <tigranv@10web.io>
2021-04-16 18:48:23 +0400

    linked list ready

--- src/components/data-structures/linked-list/linked-list.tsx
+++ src/components/data-structures/linked-list/linked-list.tsx
@@ -2,56 +2,97 @@ import React, {FC, useState} from 'react';
 import Button from '@material-ui/core/Button';
 import {TextField} from "@material-ui/core";
 
-class Node {
-    public data: object;
-    public next: object;
+class Node<T> {
+    public next: Node<T> | null = null;
+    public prev: Node<T> | null = null;
 
-    constructor(data: object = {}, next: object = {}) {
-        this.data = data;
-        this.next = next
+    constructor(public data: T) {
     }
 }
 
-class LinkedListDS {
-    private head: any;
-    private tail: Node;
+interface ILinkedList<T> {
+    append(data: T): Node<T>;
 
-    constructor() {
-        this.head = null;
-        this.tail = new Node();
-    }
+    prepend(data: T): Node<T>;
 
-    append(data: any) {
-        const node = new Node(data)
-        if (this.tail) {
-            this.tail.next = node
-        }
+    getLast(node: Node<T>): Node<T>;
+
+    deleteNode(node: Node<T>): void;
+
+    toArray(): T[];
+
+    size(): number;
+
+    search(comparator: (data: T) => boolean): Node<T> | null;
+}
+
+class LinkedListDS<T> implements ILinkedList<T> {
+    private head: Node<T> | null = null;
+
+    public prepend(data: T): Node<T> {
+        const node = new Node(data);
         if (!this.head) {
-            this.head = node
+            this.head = node;
+        } else {
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
         }
-        this.tail = node
+        return node;
     }
 
-    prepend(data: any) {
-        const node = new Node(data, this.head)
-        this.head = node
-        if (!this.tail) {
-            this.tail = node
+    public getLast = (node: Node<T>): Node<T> => {
+        return node.next ? this.getLast(node.next) : node;
+    };
+
+    public append(data: T): Node<T> {
+        const node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            const lastNode = this.getLast(this.head);
+            node.prev = lastNode;
+            lastNode.next = node;
+        }
+        return node;
+    }
+
+    public deleteNode(node: Node<T>): void {
+        if (!node.prev) {
+            this.head = node.next;
+        } else {
+            const prevNode = node.prev;
+            prevNode.next = node.next;
         }
     }
 
-    toArray() {
-        const output = []
-        let current = this.head
-
-        while (current) {
-            output.push(current)
-            current = current.next
+    public toArray(): T[] {
+        const array: T[] = [];
+        if (!this.head) {
+            return array;
         }
 
-        return output
+        const addToArray = (node: Node<T>): T[] => {
+            array.push(node.data);
+            return node.next ? addToArray(node.next) : array;
+        };
+        return addToArray(this.head);
     }
 
+    public size(): number {
+        return this.toArray().length;
+    }
+
+    public search(comparator: (data: T) => boolean): Node<T> | null {
+        const checkNext = (node: Node<T>): Node<T> | null => {
+            if (comparator(node.data)) {
+                return node;
+            }
+            return node.next ? checkNext(node.next) : null;
+        };
+
+        return this.head ? checkNext(this.head) : null;
+    }
 }
 
 const list = new LinkedListDS()
@@ -91,7 +132,7 @@ const LinkedList: FC = () => {
                 type={"submit"}
                 onClick={(e) => {
                     handleClick(e, 'append')
-                    }
+                }
                 }
             >
                 Append
