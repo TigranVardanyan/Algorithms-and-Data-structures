c49a428febde9f3da83af138c7adb027bb8d90a1
Tigran <tig.vardanyan.001@gmail.com>
2021-04-27 20:42:14 +0400

    BST - 4

--- src/classes/data-structues/Binary_search_tree_DS.js
+++ src/classes/data-structues/Binary_search_tree_DS.js
@@ -10,25 +10,53 @@ class Binary_search_tree_DS {
         } else {
             let current = this.root;
             while ( true ) {
+                if( value === current.value ) return undefined;
                 if ( value < current.value ) {
                     if ( current.left === null ) {
-                        current.left = newNode
-                        return this
-                    } else {
-                        current = current.left
+                        current.left = newNode;
+                        return this;
                     }
+                    current = current.left;
                 } else if ( value > current.value ) {
                     if ( current.right === null ) {
-                        current.right = newNode
-                        return this
-                    } else {
-                        current = current.right
+                        current.right = newNode;
+                        return this;
                     }
-                } else {
-                    return undefined
+                    current = current.right;
                 }
             }
         }
+    }
+    find(value) {
+        if ( this.root === null ) return false
+        let current = this.root;
+        let found = false;
+        while ( current && !found ) {
+            if ( value < current.value ) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else {
+                found = true
+            }
+        }
+        if ( !found ) return undefined
+        return current
+    }
+    contains(value) {
+        if ( this.root === null ) return false
+        let current = this.root;
+        let found = false;
+        while ( current && !found ) {
+            if ( value < current.value ) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
     }
 }
 
@@ -59,6 +87,12 @@ tree.insert(11)
 tree.insert(19)
 tree.insert(19)
 
-console.log(tree)
+console.log(tree.find(11))
+console.log(tree.find(20))
+console.log(tree.find(30))
+console.log(tree.find(8))
+console.log(tree.contains(8))
+
+
 
 export default Binary_search_tree_DS
