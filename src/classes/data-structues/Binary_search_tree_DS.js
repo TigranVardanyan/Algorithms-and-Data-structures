43557791378ad776d0d0740d39a94d5a0e585af7
Tigran <tig.vardanyan.001@gmail.com>
2021-04-27 20:17:42 +0400

    BST - 3

--- src/classes/data-structues/Binary_search_tree_DS.js
+++ src/classes/data-structues/Binary_search_tree_DS.js
@@ -2,6 +2,34 @@ class Binary_search_tree_DS {
     constructor() {
         this.root = null
     }
+    insert(value) {
+        let newNode = new Node(value);
+        if ( this.root === null ) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while ( true ) {
+                if ( value < current.value ) {
+                    if ( current.left === null ) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                } else if ( value > current.value ) {
+                    if ( current.right === null ) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                } else {
+                    return undefined
+                }
+            }
+        }
+    }
 }
 
 class Node {
@@ -14,13 +42,22 @@ class Node {
 
 let tree = new Binary_search_tree_DS()
 
-tree.root = new Node(10);
-
-tree.root.right = new Node(15)
-
-tree.root.left = new Node(5)
-
-tree.root.left.right = new Node(9);
+tree.insert(15)
+tree.insert(10)
+tree.insert(5)
+tree.insert(20)
+tree.insert(100)
+tree.insert(-15)
+tree.insert(0)
+tree.insert(60)
+tree.insert(21)
+tree.insert(7)
+tree.insert(8)
+tree.insert(9)
+tree.insert(56)
+tree.insert(11)
+tree.insert(19)
+tree.insert(19)
 
 console.log(tree)
 
