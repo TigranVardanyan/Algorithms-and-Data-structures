8d4fb83741ea7dbf0f43fef38f69f16268d6d45a
Tigran <tig.vardanyan.001@gmail.com>
2021-04-28 16:28:17 +0400

    Binary heaps - insert

--- src/classes/data-structues/Binary_heaps_DS.js
+++ src/classes/data-structues/Binary_heaps_DS.js
@@ -1,5 +1,35 @@
 class Binary_heaps_DS {
-
+  constructor() {
+    this.values = []
+  }
+  insert(element) {
+    this.values.push(element)
+    this.bubbleUp()
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while ( index > 0 ) {
+      let parentIndex = Math.floor((index - 1) / 2)
+      let parent = this.values[parentIndex];
+      if ( element <= parent ) break; // break loop
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
 }
+
+
+let heap = new Binary_heaps_DS();
+
+heap.insert(55);
+heap.insert(8);
+heap.insert(15);
+heap.insert(62);
+heap.insert(46);
+heap.insert(1);
+
+console.log(heap)
 
 export default Binary_heaps_DS
