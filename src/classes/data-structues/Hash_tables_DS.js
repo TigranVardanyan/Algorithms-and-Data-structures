fc51a56c7b58e2f25c64cf182b3cf014b8d212fe
Tigran <tig.vardanyan.001@gmail.com>
2021-05-02 17:04:05 +0400

    Basic hash function implementation

--- src/classes/data-structues/Hash_tables_DS.js
+++ src/classes/data-structues/Hash_tables_DS.js
@@ -10,11 +10,31 @@ class Hash_tables_DS {
         }
         return total
     }
+    hash_2(key, arrayLen) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for(let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96 // a's code
+            total = (total * WEIRD_PRIME + value) % arrayLen
+        }
+        return total
+    }
 }
 
 
+let table = new Hash_tables_DS()
 
+console.log('cyan', table.hash_2('cyan', 117));
+console.log('cyan', table.hash_2('cyan', 117));
+console.log('cyan', table.hash_2('cyan', 117));
+console.log('cyan', table.hash_2('cyan', 117));
+console.log('red', table.hash_2('red', 117));
+console.log('red', table.hash_2('red', 117));
+console.log('green', table.hash_2('green', 117));
+console.log('orange', table.hash_2('orange', 117));
+console.log('pink', table.hash_2('pink', 117));
 
-console.log(JSON.parse(JSON.stringify('132')))
+// 117
 
 export default Hash_tables_DS
---
+++ src/containers/main-content/data-structures/hash-tables/hash-tables.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import Hash_tables_DS from "../../../../classes/data-structues/Hash_tables_DS";
+
+
+const Hash_tables: FC = () => {
+
+
+    let hashTables = new Hash_tables_DS();
+    return (
+        <React.Fragment>
+            <h1> Hash tables </h1>
+
+        </React.Fragment>
+    );
+}
+
+
+export default Hash_tables;
