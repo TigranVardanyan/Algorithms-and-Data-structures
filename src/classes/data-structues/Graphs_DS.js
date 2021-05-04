248f4db691057a462d5bb73538418babc4e4db14
Tigran <tig.vardanyan.001@gmail.com>
2021-05-04 15:48:04 +0400

    Some improves in code

--- src/classes/data-structues/Graphs_DS.js
+++ src/classes/data-structues/Graphs_DS.js
@@ -27,6 +27,53 @@ class Graphs_DS {
     }
     delete this.adjacencyList[vertex]
   }
+
+  depthFirst(start){
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    //Check is there vertex
+    //Set vertex is visited in visited and push it to result
+    //Do recursive traversal of the neighbors of a vertex using the algorithm
+    //Return result
+    (function dfs(vertex){
+      if(!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          return dfs(neighbor)
+        }
+      });
+    })(start);
+
+    return result;
+  }
+
+  //return result;
+  //while queue is not empty
+  //get and remove first element from queue
+  //push it to result
+  //traverse the array and set not visited neighbor true and push it to queue
+  breadthFirst(start){
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while(queue.length){
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result
+  }
 }
 
 export default Graphs_DS;
